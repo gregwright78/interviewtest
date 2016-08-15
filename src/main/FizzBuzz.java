52f586fc2aa88262a13457c2eb865d602441dfbc
gregwright78@gmail.com <gregwright78@gmail.com>
2016-08-15 11:59:16 +0100

    FizzBuzz - updated test and corrected bugs in implementation class

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -7,12 +7,12 @@ public class FizzBuzz {
 	public static ArrayList<String> generateStringArray(int[] testIntArray) {
 		ArrayList<String> arrayListResult = new ArrayList<String>();		
 		for (int i=0; i<testIntArray.length; i++) {
-			if (testIntArray[i]%3==0) {
-				arrayListResult.add("Fizz");
+			if (testIntArray[i]%3==0 && testIntArray[i]%5==0) {
+			arrayListResult.add("FizzBuzz");
 			} else if (testIntArray[i]%3==0) {
+				arrayListResult.add("Fizz");
+			} else if (testIntArray[i]%5==0) {
 				arrayListResult.add("Buzz");
-			} else if (testIntArray[i]%3==0 && testIntArray[i]%3==0) {
-				arrayListResult.add("Fizz Buzz");
 			} else {
 				arrayListResult.add(new Integer(testIntArray[i]).toString());
 			}
--- src/test/TestFizzBuzz.java
+++ src/test/TestFizzBuzz.java
@@ -12,12 +12,21 @@ public class TestFizzBuzz {
 
 	@Test
 	public void testFizzBuzz() {
-		int[] testIntArray = {3,5,6};
+		int[] testIntArray = {3,5,6,9,15,16};
 		ArrayList<String> expectedResult = new ArrayList<String>();
 		expectedResult.add("Fizz");
 		expectedResult.add("Buzz");
 		expectedResult.add("Fizz");
+		expectedResult.add("Fizz");
+		expectedResult.add("FizzBuzz");
+		expectedResult.add("16");
+		
 		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(0), expectedResult.get(0));
+		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(1), expectedResult.get(1));
+		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(2), expectedResult.get(2));
+		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(3), expectedResult.get(3));
+		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(4), expectedResult.get(4));
+		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(5), expectedResult.get(5));
 	}
 
 }
