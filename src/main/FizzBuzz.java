7a87470baf0cbca62faf6c3f37f7eaacf6f21469
gregwright78@gmail.com <gregwright78@gmail.com>
2016-08-15 11:32:59 +0100

    added test for a basic fizzbuzz test

---
+++ src/main/FizzBuzz.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.ArrayList;
+
+public class FizzBuzz {
+
+	public static ArrayList<String> generateStringArray(int[] testIntArray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
