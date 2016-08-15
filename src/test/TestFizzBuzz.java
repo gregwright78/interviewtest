4baedd9e070b7bdbbc1819f9a7b86eec53d0f5a3
gregwright78@gmail.com <gregwright78@gmail.com>
2016-08-15 12:10:18 +0100

    FizzBuzz - added additional test method including output looping from numbers 1 to 20

--- src/test/TestFizzBuzz.java
+++ src/test/TestFizzBuzz.java
@@ -28,5 +28,36 @@ public class TestFizzBuzz {
 		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(4), expectedResult.get(4));
 		assertEquals(FizzBuzz.generateStringArray(testIntArray).get(5), expectedResult.get(5));
 	}
+	
+	@Test
+	public void testFizzBuzz1to20() {
+		int[] testIntArray = new int[20];
+		for (int i=0; i<20; i++) {
+			testIntArray[i]=i+1;
+		}
+		
+		ArrayList<String> expectedResult = new ArrayList<String>();
+		expectedResult.add("1");
+		expectedResult.add("2");
+		expectedResult.add("Fizz");
+		expectedResult.add("4");
+		expectedResult.add("Buzz");
+		expectedResult.add("Fizz");
+		expectedResult.add("7");
+		
+		ArrayList<String> ResultStringArray = FizzBuzz.generateStringArray(testIntArray);
+		
+		assertEquals(ResultStringArray.get(0), expectedResult.get(0));
+		assertEquals(ResultStringArray.get(1), expectedResult.get(1));
+		assertEquals(ResultStringArray.get(2), expectedResult.get(2));
+		assertEquals(ResultStringArray.get(3), expectedResult.get(3));
+		assertEquals(ResultStringArray.get(4), expectedResult.get(4));
+		assertEquals(ResultStringArray.get(5), expectedResult.get(5));
+		assertEquals(ResultStringArray.get(6), expectedResult.get(6));
+		
+		for (int i=0; i<20; i++) {
+			System.out.println(ResultStringArray.get(i));
+		}
+	}
 
 }
